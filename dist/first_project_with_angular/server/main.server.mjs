973dd6916b2d02404269e652b53f0d10433a2688
wnomena <wnomena58@gmail.com>
2024-10-21 08:08:39 +0300

    dist

---
+++ dist/first_project_with_angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-R6YTELJF.mjs";import"./chunk-7FE4CY7X.mjs";import"./chunk-GGQXRXAC.mjs";import"./chunk-KFNPEPC2.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
